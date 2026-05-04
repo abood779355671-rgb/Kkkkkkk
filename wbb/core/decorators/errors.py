815b2118a4cf827586dbb4284c364e55c6bf059e
abood779355671-rgb <abood779355671@gmail.com>
2026-05-05 02:26:24 +0300

    Create errors.py

---
+++ wbb/core/decorators/errors.py
@@ -0,0 +1,37 @@
+import traceback
+from functools import wraps
+from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
+from wbb import LOG_GROUP_ID, app
+
+def split_limits(text):
+    if len(text) < 2048: return [text]
+    lines = text.splitlines(True)
+    small_msg = ""
+    result = []
+    for line in lines:
+        if len(small_msg) + len(line) < 2048:
+            small_msg += line
+        else:
+            result.append(small_msg)
+            small_msg = line
+    else:
+        result.append(small_msg)
+    return result
+
+def capture_err(func):
+    @wraps(func)
+    async def capture(client, message, *args, **kwargs):
+        try:
+            return await func(client, message, *args, **kwargs)
+        except ChatWriteForbidden:
+            await app.leave_chat(message.chat.id)
+            return
+        except Exception as err:
+            errors = traceback.format_exc()
+            error_feedback = split_limits(
+                f"**خطأ** | من: `{message.from_user.id if message.from_user else 0}` | دردشة: `{message.chat.id if message.chat else 0}`\n\n```{message.text or message.caption}```\n\n```{''.join(errors)}```"
+            )
+            for x in error_feedback:
+                await app.send_message(LOG_GROUP_ID, x)
+            raise err
+    return capture
